c6c7816a8723ff82b9c62ce11f313b0409b3255e
BoomanWill <williamhstanger@btinternet.com>
2022-01-04 21:51:16 +0000

    added files

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pip install tk
+pip install os
+pip install subprocess
+pip install pyyaml
+
+sudo mkdir ~/.rhythmboxcontrols
+sudo mv images ~/.rhythmboxcontrols
+sudo mv musiccontrols.py ~/.rhythmboxcontrols
+sudo mv config.yml ~/.rhythmboxcontrols
+sudo mv musiccontrols.desktop /usr/share/applications
+sudo mv musiccontrols /usr/bin
+sudo chmod +x /usr/bin/musiccontrols
